93ccb4ff89b213118c2b2e9598c35f4e94ff34f5
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-07-04 14:47:24 +0300

    feat: add  get requests to router

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const Transactions = require('../../model')
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await Transactions.listTransaction()
+    return res.json({ status: 'success', code: 200, data: { result } })
+  } catch (e) {
+    next(e)
+  }
 })
 
 router.get('/:transactionId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await Transactions.getTransactionById(
+      req.params.transactionId
+    )
+    if (result) {
+      return res.json({ status: 'success', code: 200, data: { result } })
+    }
+    return res.json({ status: 'error', code: 404, message: 'Not found' })
+  } catch (e) {
+    next(e)
+  }
 })
 
 router.post('/', async (req, res, next) => {
