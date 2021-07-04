eebd24c65a7a4159f8bb6c5c14a64216d4e5eb85
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-07-04 23:13:10 +0300

    feat: add controllers

---
+++ controllers/transactions.js
@@ -0,0 +1,80 @@
+const Transaction = require('../repositories/transactions')
+
+const getAll = async (_req, res, next) => {
+  try {
+    const result = await Transaction.listTransaction()
+    return res.json({ status: 'success', code: 200, data: { result } })
+  } catch (e) {
+    next(e)
+  }
+}
+
+const addTransaction = async (req, res, next) => {
+  try {
+    const contact = await Transaction.addContact(req.body)
+    return res
+      .status(201)
+      .json({ status: 'success', code: 201, data: { contact } })
+  } catch (e) {
+    next(e)
+  }
+}
+
+const removeTransaction = async (req, res, next) => {
+  try {
+    const result = await Transaction.removeTransaction(req.params.transactionId)
+    if (result) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        message: 'Contact deleted',
+        data: { result },
+      })
+    }
+    return res.json({ status: 'error', code: 404, message: 'Not found' })
+  } catch (e) {
+    next(e)
+  }
+}
+
+const getTransactionById = async (req, res, next) => {
+  try {
+    const result = await Transaction.getTransactionById(
+      req.params.transactionId
+    )
+    if (result) {
+      console.log(result)
+      return res.json({ status: 'success', code: 200, data: { result } })
+    }
+    return res.json({ status: 'error', code: 404, message: 'Not found' })
+  } catch (e) {
+    next(e)
+  }
+}
+
+const updateTransaction = async (req, res, next) => {
+  try {
+    const result = await Transaction.updateTransaction(
+      req.params.transactionId,
+      req.body
+    )
+    if (result) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        data: { result },
+      })
+    }
+    return res.json({ status: 'error', code: 404, message: 'Not found' })
+  } catch (e) {
+    next(e)
+  }
+}
+
+module.exports = {
+  getAll,
+  addTransaction,
+  removeTransaction,
+  getTransactionById,
+  updateTransaction,
+}
