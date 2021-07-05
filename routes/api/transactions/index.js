6c8e77233c10dc879210317debb0ec27903755e6
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-07-05 23:29:25 +0300

    refactor: create transactions folder in api

--- routes/api/transactions/index.js
+++ routes/api/transactions/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const controllers = require('../../controllers/transactions')
+const controllers = require('../../../controllers/transactions')
 
 router.get('/', controllers.getAll).post('/', controllers.addTransaction)
 
