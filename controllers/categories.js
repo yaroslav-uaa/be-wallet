32979d0d4461eb302b4f2c7423dd4723cafe0154
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-07-06 22:17:05 +0300

    feat: add calculate income & consumption

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,7 +2,6 @@ const Categories = require('../repositories/categories')
 
 const getCategories = async (req, res, next) => {
   try {
-    console.log(req)
     const { month, year } = req.query
     const result = await Categories.getCategories(month, year)
     return res.json({ status: 'success', code: 200, data: { result } })
--- repositories/categories.js
+++ repositories/categories.js
@@ -3,15 +3,23 @@ const Transaction = require('../model/transaction')
 const getCategories = async (month, year) => {
   const startDate = `${year}-${month}-01`
   const endDate = configureEndDate(month, year)
-  console.log(startDate)
-  console.log(endDate)
-  const result = await Transaction.find({
+
+  const transactions = await Transaction.find({
     date: { $gte: startDate, $lt: endDate },
   })
-  return result
+
+  const income = calculateIncome(transactions)
+  const consumption = calculateConsumption(transactions)
+
+  return {
+    income: income,
+    consumption: consumption,
+    balance: income - consumption,
+  }
 }
+
 // TODO: узнать  0 или 1 январь в дате при выборе в статистике
-// ****функция для расчета:
+// ****функция для:
 // если выбирают декабрь, то конечная дата периода - 1 января СЛЕДУЮЩЕГО ГОДА
 const configureEndDate = (month, year) => {
   if (month === '12') {
@@ -19,6 +27,28 @@ const configureEndDate = (month, year) => {
   } else {
     return `${year}-${parseInt(month) + 1}-01`
   }
+}
+
+// ****функция для расчета: ДОХОДА
+const calculateIncome = (transactions) => {
+  const incomeArray = transactions.filter((el) => el.income)
+
+  const income = incomeArray.reduce(
+    (total, el) => (total += parseInt(el.sum)),
+    0
+  )
+  return income
+}
+
+// ****функция для расчета: РАСХОДА
+const calculateConsumption = (transactions) => {
+  const consumptionArray = transactions.filter((el) => !el.income)
+  const consumption = consumptionArray.reduce(
+    (total, el) => (total += el.sum),
+    0
+  )
+  console.log(consumption)
+  return consumption
 }
 
 module.exports = { getCategories }
