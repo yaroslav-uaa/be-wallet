b1f0d52d171fa2f483611adcd2206420521f9fca
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-07-22 10:31:28 +0300

    feat: add getCapital route

--- routes/api/transactions/capital.js
+++ routes/api/transactions/capital.js
@@ -5,5 +5,6 @@ const guard = require('../../../helpers/guard')
 const controllerCapital = require('../../../controllers/capital')
 
 router.post('/', guard, controllerCapital.addCapital)
+router.get('/sum', guard, controllerCapital.getCapital)
 
 module.exports = router
